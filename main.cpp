0192511eafd72d1e21d73f411f30a7449c0562aa
G0Gi-cw <gogolyakpeter@gmail.com>
2024-11-23 14:37:53 +0100

    errorFIxComment

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS helyett
+    std::cout << '1-100 ertekek duplazasa'//expected ;
+    for (int i = 0;)//rossz szintaktika
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//Érték üres pontos vessző hiánya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//Ki kell nullázni ls int helyxett float
+    for (int i = 0; i < N_ELEMENTS, i++)//for loop szintaktikai hhba
     {
-        atlag += b[i]
+        atlag += b[i]//pontos vessző hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0;//Dinamikus memoria felszabaditása
 }
