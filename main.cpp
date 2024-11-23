eed46bfa74dbd88c8508e827d5f8d84aa24f1f5c
G0Gi-cw <gogolyakpeter@gmail.com>
2024-11-23 14:39:59 +0100

    realErrorFixEpic

--- main.cpp
+++ main.cpp
@@ -4,23 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS helyett
-    std::cout << '1-100 ertekek duplazasa'//expected ;
-    for (int i = 0;)//rossz szintaktika
+    int *b = new int[N_ELEMENTS]; //N_ELEMENTS NELEMENTS helyett
+    std::cout << "1-100 ertekek duplazasa";  // expected ;
+    for (int i = 0; i<N_ELEMENTS; i++) // for loop szintaktikai hiba int i=0; i<N_ELEMENTS; i++
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"//Érték üres pontos vessző hiánya
+        std::cout << "Ertek:"<<i; // érték üres és expected ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;//Ki kell nullázni ls int helyxett float
-    for (int i = 0; i < N_ELEMENTS, i++)//for loop szintaktikai hhba
+    int atlag; // átlag init 0 és float használata célszerűbb
+    for (int i = 0; i < N_ELEMENTS; i++) //for loop szintaktikai hiba ,->;
     {
-        atlag += b[i]//pontos vessző hiánya
+        atlag += b[i]; //expected ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;//Dinamikus memoria felszabaditása
+    return 0;
+    delete[] b; 
+    std::cout << "Itt jart szmore " << std::endl;
+    //dinamikusan lefoglat memóriát törölni kell
 }
